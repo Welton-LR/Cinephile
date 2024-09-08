7828a24bd16413d8c377ac81192388b7149bf836
Welton <weltonrdesigner@gmail.com>
2024-09-07 21:18:19 -0300

    alterando os cards dinâmicos

--- cinephile/src/components/Card/index.js
+++ cinephile/src/components/Card/index.js
@@ -3,7 +3,7 @@ import styles from "./Card.module.css"
 function Card({ id, imageUrl}){
     return(
         <section className={styles.card}>
-            <a href={ `https://youtu.be/5UVaA6SGRZA?si=${id}`}target="_blank"  rel="noopener noreferrer">
+            <a href={id}target="_blank"  rel="noopener noreferrer">
             <img src={imageUrl} alt="capa"></img> 
             </a>
         </section>
