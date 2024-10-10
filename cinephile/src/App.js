85e3c5ecef122e8126663e2ca7540bb95e20fdd3
Welton <weltonrdesigner@gmail.com>
2024-10-09 22:23:11 -0300

    Finalizei o componente Category

--- cinephile/src/App.js
+++ cinephile/src/App.js
@@ -6,29 +6,48 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import videos from "./json/movieDB.json";
 
+const categories = [
+  "Sci-Fi",
+  "Terror",
+  "Animation"
+]
+
+function filterCategory(id){
+  return(
+    videos.filter(video => video.category === categories[id])
+  )
+}
+
 function App() {
   return (
     <>
       <Header/>
       <Banner image="/images/bannerCinephile.png"/>
       <Container>
-        {/* <h1>Animation</h1>
-        <section className="cards">
-          {
-            videos.map((video =>{
-              return <Card id={video.id} key={video.id} imageUrl={video.imageUrl}/>
-            }))
-          }
-        </section> */}
+
+        {/*Aqui eu pego a minha variável "videos" e faço um mapeamento de cada elemento contido nela.
+                Cada elemento será chamado de "video" e para cada video, será retornada uma função que chama 
+                o componente Card. O Card cria uma card para cada elemento em "videos" */}
+
         <Category category = "Sci-Fi">
               {
-                /*Aqui eu pego a minha variável "videos" e faço um mapeamento de cada elemento contido nela.
-                Cada elemento será chamado de "video" e para cada video, será retornada uma função que chama 
-                o componente Card. O Card cria uma card para cada elemento em "videos" */
-
-                  videos.map((video =>{
-                    return <Card id={video.id} key={video.id} imageUrl={video.imageUrl}/>
-                  }))
+                  filterCategory(0).map((video =>{
+                     return <Card id={video.id} key={video.id} imageUrl={video.imageUrl}/>
+                   }))
+              }
+        </Category>
+        <Category category = "Terror">
+              {
+                   filterCategory(1).map((video =>{
+                     return <Card id={video.id} key={video.id} imageUrl={video.imageUrl}/>
+                   }))
+              }
+        </Category>
+        <Category category = "Animation">
+              {
+                   filterCategory(2).map((video =>{
+                     return <Card id={video.id} key={video.id} imageUrl={video.imageUrl}/>
+                   }))
               }
         </Category>
 
